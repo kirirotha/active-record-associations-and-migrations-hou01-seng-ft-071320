93c81c1f90557fbe8af2130ca734956d8552630e
Kiri Um <kirirotha@gmail.com>
2020-07-27 13:41:02 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,3 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
   class CreateArtists < ActiveRecord::Migration[4.2]
     def change
       create_table :artists do |t|
@@ -6,4 +5,4 @@ class CreateArtists < ActiveRecord::Migration[4.2]
       end
     end
   end
-end
+
